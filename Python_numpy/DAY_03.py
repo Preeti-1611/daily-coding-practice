37ced200cd2b3524a5608636950f86e512734f7a
Preeti-1611 <preetinagarale16@gmail.com>
2025-10-30 19:14:56 +0530

    Added numpy  copy_view

--- Python_numpy/DAY_03.py
+++ Python_numpy/DAY_03.py
@@ -31,3 +31,44 @@ print("new array datatype:", newarr.dtype)
 | How to convert a float array to integer?                   | Using `.astype(int)` method.                               |
 | Difference between `'i4'` and `'i8'`?                      | `'i4'` → 4 bytes (32-bit), `'i8'` → 8 bytes (64-bit).      |
 '''
+# copy and view
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+
+arr[0] = 42
+print(arr)  # [42  2  3  4  5]
+print(x)    # [1  2  3  4  5]
+
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+
+arr[0] = 42
+print(arr)  # [42  2  3  4  5]
+print(x)    # [42  2  3  4  5]
+# True → it’s a view (shares data)
+# False → it’s a copy (independent)
+
+print(x.base is arr)
+
+
+arr = np.array([10, 20, 30])
+x = arr.view()
+y = arr.copy()
+
+arr[1] = 99
+
+print("arr:", arr)  # [10 99 30]
+print("x:", x)      # [10 99 30]  (view changes)
+print("y:", y)      # [10 20 30]  (copy stays same)
+
+'''| Question                                                    | Short Answer                                                 |
+| ----------------------------------------------------------- | ------------------------------------------------------------ |
+| What is the difference between `copy()` and `view()`?       | `copy()` creates a new array, `view()` shares the same data. |
+| If you modify the original array, will `copy()` reflect it? | No.                                                          |
+| If you modify the original array, will `view()` reflect it? | Yes.                                                         |
+| How to check if an array owns its data?                     | `arr.base is None` → means it owns the data (not a view).    |
+| Which uses less memory?                                     | `view()`                                                     |
+'''
