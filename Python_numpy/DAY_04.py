ee565fd17c36e02880828adc613556915cb1f52d
Preeti-1611 <preetinagarale16@gmail.com>
2025-10-31 20:48:20 +0530

    worked on matrix

---
+++ Python_numpy/DAY_04.py
@@ -0,0 +1,41 @@
+from numpy import *
+'''a1 =array([2,3,4,5,6,7])
+print(sqrt(a1))
+# print(sqet(a1))
+print(sum(a1))
+print(min(a1))
+print(max(a1))
+print(mean(a1))
+print(median(a1))
+print(sin(a1))
+print(cos(a1))
+
+
+print(log(a1))
+
+#conactenation'
+arr1 = array([1,2,3,4])
+arr2 = array([5,6,7,8,9])
+print(concatenate((arr1,arr2)))
+
+'''
+#same ouput
+'''b1 =array([2,3,4,5,6])
+b2 =b1
+print(b1)   # [2 3 4 5 6]
+             #[2 3 4 5 6]
+print(b2)
+
+print(id(b1))   #same id
+print(id(b2))
+
+'''
+d1=array([3,4,5])
+d2=d1.copy()
+print(d1)
+print(d2)
+d1[2]=23
+print(d1)
+print(d2)
+d2[1]=34
+print(d2)
