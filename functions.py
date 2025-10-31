fbaf77c8834a62b24e8e2761246ed78086fecf06
Preeti-1611 <preetinagarale16@gmail.com>
2025-10-31 23:16:04 +0530

    added functions

---
+++ functions.py
@@ -0,0 +1,20 @@
+def greet():
+    # print("hello")
+    return "Hello, World!"
+
+print(greet())
+
+
+def add(a,b):
+    return a+b
+m=add(3,4)
+print(m)
+
+def mul_sub(x,y,z):
+    c = x*y*z
+    d =x-y-z
+    return c,d
+m1,m2 =mul_sub(3,4,6)
+print(m1,m2)
+
+
