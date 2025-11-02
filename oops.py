caae6b7649599158647e1d4cddc87fc898c49284
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-02 23:25:06 +0530

    started oops

---
+++ oops.py
@@ -0,0 +1,29 @@
+'''class computer:
+    def config(self):
+        print("hello everyone this is I5,16gb")
+
+comp1 = computer()
+comp2 = computer()
+print(type(comp1))
+#call
+# computer.config(comp1)
+# computer.config(comp2)
+comp1.config()
+comp2.config()
+
+'''
+
+class dog:
+    print("hello")
+    
+d1 =dog()
+print(d1)
+print(type(d1))
+
+
+
+
+         
+
+
+
