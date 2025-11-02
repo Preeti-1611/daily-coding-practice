7eb4479514050a31aa175679efa9e03429e443ad
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-02 23:26:09 +0530

    class and object

---
+++ oops1.py
@@ -0,0 +1,32 @@
+'''class dance:
+
+    def __init__(self,name,no):
+        self.name = name
+        self.no = no
+        print("in init")
+    
+        
+
+    def d1(self):
+        print("this is d1 class")
+
+me1 = dance('preeti','001')
+
+me2 = dance( 'adarsh','002')
+
+me1.d1()
+me2.d1()
+'''
+class computer:
+     pass
+
+
+c1 =computer()
+c2 =computer()
+print(id(c1))
+print(id(c2))
+
+
+
+
+
