8225b621fe4b76310fb599f31a6cfc4d31e37dd3
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-22 22:43:02 +0530

    praticed problems

---
+++ complete-coding-concepts/T-01.py
@@ -0,0 +1,19 @@
+print(3+1)
+print(3-2)
+print(3*2)
+print(3**2)
+print(3//2)
+print(3/2)
+
+
+
+#data types
+print(type(10))
+print(type(3.54))
+print(type('3r'))
+print(type([2,3,4]))
+print(type({ 'name':'preeti'}))
+print(type((3.5,5,66)))
+print(type({2,3,5,3}))
+print(type(3>2))
+print(type(3==3))
