7d57bdef6a7d377ceccc06b3ae7d0d6d5bdb53f5
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-27 21:47:21 +0530

    Praticed concepts

---
+++ complete-coding-concepts/T-02.py
@@ -0,0 +1,55 @@
+#casting
+
+#int to float
+# num_int = 10
+# print("num_int",num_int)
+# num_float = float(num_int)
+# print("num_float",num_float)
+
+
+# #float to int
+
+# gravity = 9.81
+# print(int(gravity))
+
+
+#int to str
+# num_int =20
+# num_str = str(num_int)
+# print("num_str",num_str)
+
+#str to int
+num_str = '10'
+num_int = int(num_str)
+print("num_int",num_int)
+
+
+#str to list
+
+first_name = 'Preeti'
+print(first_name)
+first_name_to_list = list(first_name)
+print(first_name_to_list)
+
+# python programming
+first_name = "Preeti"
+last_name = "Nagarale"
+full_name = "preeti nagarale"
+country = "india"
+city ="xyz"
+age  = "333"
+print(type(age))
+print(len(first_name),len(last_name))
+
+
+num_1 = 5
+num_2 =4
+total = num_1 +num_2
+print(total)
+print("subtraction",num_1-num_2)
+print("multiplaction",num_1*num_2)
+print("divide",num_1/num_2)
+print("modulus",num_1%num_2)
+exp = pow(num_1,num_2)
+print(exp)
+
