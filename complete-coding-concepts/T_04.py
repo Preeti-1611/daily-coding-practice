bbe98959bedb80157f3bb7f83b27b086e34b29e5
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-29 21:02:50 +0530

    Modified

---
+++ complete-coding-concepts/T_04.py
@@ -0,0 +1,6 @@
+# Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+# Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+# Calculate the slope, x-intercept and y-intercept of y = 2x -2
+# Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+
+
